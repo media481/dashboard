c0bc0fece2690008b54dd833636f63fe0c9dbba1
Amirubase <media@sih.or.id>
2026-07-25 12:52:57 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // ============================================================
 // 1. SUPABASE CONFIG
 // ============================================================
-const SUPABASE_URL = "https://rkdhssbyqqyheczejtix.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_YzVUaQ-f53v3JId4art8zg_AQWSSMU_";
+const SUPABASE_URL = "https://asfcqbwvxomkcqzdkshf.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzZmNxYnd2eG9ta2NxemRrc2hmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4OTcwNzIsImV4cCI6MjEwMDQ3MzA3Mn0.I00yrEnePXDsuZgMpZyU5HVSzRgN4_cCKNxfXv8WdXQ";
 const MASKAPAI_LIST = ["Oman Air","Saudia Airlines","Lion Air","Garuda Indonesia","Emirates","Qatar Airways","Etihad Airways","Malindo Air","Air Asia","IndiGo"];
 const CACHE_KEY = 'amiru_cached_data';
 const CACHE_TIME_KEY = 'amiru_cache_time';
